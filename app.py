92edcf68c5e329c2fd75d641d2d859658877997e
Kushal Dave <kushaldave02@gmail.com>
2025-03-27 20:46:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,6 @@ from fpdf import FPDF
 from collections import Counter
 
 
-import torch
-from ultralytics.nn.tasks import DetectionModel  # Import the correct class
-from torch import nn  # Import necessary components from PyTorch
-
-# Ensure to add only safe globals needed for loading models
-torch.serialization.add_safe_globals([DetectionModel, nn.Module, nn.Sequential])
-
-
-
-
-
 
 
 app = Flask(__name__, static_folder='static')
@@ -187,4 +176,5 @@ def download_report():
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], 'report.pdf'), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port, debug=False)  # Use debug=False for production
